f4585b3202534198663b6a7f7b5d15d255121f63
malek1303 <malek0002@gmail.com>
2025-09-06 00:06:41 +0300

    Port Drivers

---
+++ Drivers/MCAL/DIO/PORT/PORT_prog.c
@@ -0,0 +1,18 @@
+#include "STD_TYPES.h"
+
+#include "PORT_config.h"
+#include "PORT_priv.h"
+#include "PORT_int.h"
+
+void PORT_voidInit(void)
+{
+	DDRA_Register=PORTA_DIR;
+	DDRB_Register=PORTB_DIR;
+	DDRC_Register=PORTC_DIR;
+	DDRD_Register=PORTD_DIR;
+	
+	PORTA_Register=PORTA_INITIAL_VALUE;
+	PORTB_Register=PORTB_INITIAL_VALUE;
+	PORTC_Register=PORTC_INITIAL_VALUE;
+	PORTD_Register=PORTD_INITIAL_VALUE;
+}
